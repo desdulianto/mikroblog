7d4e17e33be9ca94957df655103191f91c79d99a
Des Dulianto <desdulianto@waruna-group.co.id>
2016-09-10 10:43:56 +0700

    pindahkan lokasi konifgurasi ke directory instance

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 # application setup
 app = Flask(__name__)
 app.config.from_object('config.Default')
-app.config.from_pyfile('config.cfg', silent=True)
+app.config.from_pyfile('instance/config.cfg', silent=True)
 
 # connect to db
 mongo = PyMongo(app)
