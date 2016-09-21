65d92933fb0282aa144409e96af59f3ed55aa147
Desdulianto <desdulianto@waruna-group.co.id>
2016-09-21 16:22:40 +0700

    tambahkan mongodb dan user/password default admin

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from model import mongo
 
 # application setup
 app = Flask(__name__)
@@ -7,7 +8,23 @@ app.config.from_object('config.Default')
 app.config.from_pyfile('instance/config.cfg', silent=True)
 
 # connect to db
-mongo = PyMongo(app)
+mongo.init_app(app)
+
+# initialize db
+@app.before_first_request
+def init_db():
+    # create collections
+    if 'users' not in mongo.db.collection_names():
+        mongo.db.create_collection('users')
+    if 'posts' not in mongo.db.collection_names():
+        mongo.db.create_collection('posts')
+
+    # add admin user if not exists
+    admin = mongo.db.users.find_one({'username':'admin'})
+    # password hash using sha256sum
+    if admin is None:
+        mongo.db.users.insert_one(dict(id=1, username='admin',
+            password='8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'))
 
 @app.route('/')
 def index():
